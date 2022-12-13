9985352514c879bea5eb46fe8b3b2812069edaaa
Husain <91032389+husainbismil@users.noreply.github.com>
2022-12-13 21:27:23 +0000

    removed 'require controller' from model
    removed 'require controller' from model as its not needed there

--- model/model.js
+++ model/model.js
@@ -1,6 +1,4 @@
-// c = controller, m = model, v = view, db = database
 const db = require("../db/connection.js");
-const c = require("../controller/controller.js");
 
 exports.selectTopics = () => {
     return db.query("SELECT * FROM topics").then((queryResult) => {
