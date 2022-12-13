5a8b5e99a34e65dfe7bb7e19b78d1eec858e1260
Husain <91032389+husainbismil@users.noreply.github.com>
2022-12-13 21:49:31 +0000

    fixing test suite after previous changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,7 +20,7 @@ describe("NCNews-Server Unit Tests", () => {
         test("Responds with an array of objects, where the objects have at least two properties: slug & description", () => {
 
             return request(app).get("/api/topics").expect(200).then((response) => {
-                const topics = response.body;
+                const topics = response.body.topics;
 
                 topics.forEach((topicObject) => {
                     expect(topicObject).toEqual(
