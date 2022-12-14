4d7d28e3f89bc95fefb159f284c877807d7d1e0e
Husain Bismil <husain@upfrontideas.net>
2022-12-14 11:57:34 +0000

    adding in kyle fixes

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,12 @@
 // c = controller, m = model, v = view, db = database
-const m = require("../model/model");
+const model = require("../model/model");
 
 // TODO: find out how error catching is meant to be, repeating .catch here seems not very DRY
 
-// /api/topics Endpoint
+// /api/topics Endpoints
 exports.getApiTopics = (request, response) => {
-    m.selectTopics().then((selectTopicsQueryResult) => {
-        const responseObject = {topics: selectTopicsQueryResult.rows};
-        response.status(200).send(responseObject);
+    model.selectTopics().then((selectTopicsResponseObject) => {
+        response.status(200).send(selectTopicsResponseObject);
     }).catch((err) => {
         console.log(err);
     });
--- model/model.js
+++ model/model.js
@@ -1,7 +1,8 @@
 const db = require("../db/connection.js");
 
 exports.selectTopics = () => {
-    return db.query("SELECT * FROM topics").then((queryResult) => {
-        return queryResult;
+    return db.query("SELECT * FROM topics;").then((selectTopicsQueryResult) => {
+        const selectTopicsResponseObject = {topics: selectTopicsQueryResult.rows};
+        return selectTopicsResponseObject;
     });
-  };
+};
