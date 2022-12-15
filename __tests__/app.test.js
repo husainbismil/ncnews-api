9199ae1f6274021a75b19fe6fdf3d79a420dc1d8
Husain Bismil <husain@upfrontideas.net>
2022-12-15 14:57:55 +0000

    adding in stephen fixes for this branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -152,56 +152,7 @@ describe(`NCNews-Server Unit Tests`, () => {
 
     });
 
-    describe(`GET /api/articles/`, () => {
-
-        test(`[ 200 ] Responds with an array of objects, where the objects have the following 7 properties: author, title, article_id, topic, created_at, votes, comment_count`, () => {
-
-            return request(app).get(`/api/articles`).expect(200).then((response) => {
-                const articlesArray = response.body.articles;
-                expect(articlesArray).toHaveLength(12);
-
-                articlesArray.forEach((articleObject) => {
-                    expect(articleObject).toEqual(
-                        expect.objectContaining({
-                            author: expect.any(String),
-                            title: expect.any(String),
-                            topic: expect.any(String),
-                            article_id: expect.any(Number),
-                            created_at: expect.any(String),
-                            votes: expect.any(Number),
-                            comment_count: expect.any(Number)
-                    }));
-                });
-            });
-        });
-
-        test(`[ 200 ] Responds with an array of objects, where the objects are sorted by Date using the 'created_at' property in Descending order (latest dates first)`, () => {
-
-            return request(app).get(`/api/articles`).expect(200).then((response) => {
-                const articlesArray = response.body.articles;   
-                expect(articlesArray).toHaveLength(12);
-
-                articlesArray.forEach((e, index) => {
-                    if (index < articlesArray.length - 1) {
-                    expect(Date.parse(articlesArray[index][`created_at`]) > Date.parse(articlesArray[index + 1][`created_at`])).toBe(true);
-                }});
-            });
-
-        });
-
-           
-    });
-
-    // Error Handling
-    describe("Error Handling Tests", () => {
-        // GET METHOD 404 ERROR TEST
-        test("[ 404 ] Responds with a 404 error when an invalid path is specified", () => {
-          return request(app).get("/sdfhdshifsdhfsd").expect(404).then((response) => {
-              expect(response.body).toEqual({ error: "Error 404 File Not Found" });
-            });
-        });
-        // need 500 internal server error test - find out what to do for that one
-    });
-
+    
+    
 // End Unit Tests
 });
