81c3bd21d09ad120e2be605eaea1d2d625aa1709
Husain Bismil <husain@upfrontideas.net>
2022-12-15 09:54:18 +0000

    adding in stephen fixes from prev branch

--- model/model.js
+++ model/model.js
@@ -8,14 +8,10 @@ const selectTopics = () => {
 };
 
 const selectArticles = () => {
-    const sqlQuery = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comments.*) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`
+    const sqlQuery = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, CAST(COUNT(comments.*) AS int) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`
 
     return db.query(sqlQuery).then((queryResult) => {
-        // Convert values to correct formats
-        queryResult.rows.forEach((element) => {
-            element["comment_count"] = Number(element["comment_count"]);
-        });
-
+        
         const responseObject = {articles: queryResult.rows};
         return responseObject;
     });
